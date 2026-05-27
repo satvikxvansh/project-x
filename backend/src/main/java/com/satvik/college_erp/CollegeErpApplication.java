7a35a80ff0b371fee30c46cf549699b92ad096e5
Satvik Vansh (BIT) <satvikvansh@gmail.com>
2026-05-27 22:40:47 +0530

    chore: Spring Boot setup

---
+++ backend/src/main/java/com/satvik/college_erp/CollegeErpApplication.java
@@ -0,0 +1,13 @@
+package com.satvik.college_erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeErpApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/satvik/college_erp/CollegeErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satvik.college_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
